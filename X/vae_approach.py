834d084b21db5744964141ee6dd45635eca48ec0
TheNonEconomist <thenoneconomist@gmail.com>
2024-04-28 02:42:46 -0400

    X

--- X/vae_approach.py
+++ X/vae_approach.py
@@ -1,16 +1,27 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models, regularizers
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras import Model
 from keras import backend as K
 import numpy as np
-import math, os
+import math, os, cv2, argparse
+
 
 from dotenv import load_dotenv
 load_dotenv()
+
+import image_resizing as img
 
 ok_pic_formats = {"jpg", "jpeg", "webp", "png"}
 
 POISON_PIC_LOCATION = os.getenv("NIGHTSHADE_PIC_LOCATION")
 REGULAR_PIC_LOCATION = os.getenv("REGULAR_PIC_LOCATION")
+HOMEMADE_POISON_PIC_LOCATION = os.getenv("HOMEMADE_POISON_PIC_LOCATION")
+
+RESIZED_REGULAR_PIC_FILE_NAME = os.getenv("RESIZED_REGULAR_PIC_FILE_NAME")
+PADDED_REGULAR_PIC_FILE_NAME = os.getenv("PADDED_REGULAR_PIC_FILE_NAME")
+
+RESIZED_POISONED_PIC_FILE_NAME = os.getenv("RESIZED_POISONED_PIC_FILE_NAME")
+PADDED_POISONED_PIC_FILE_NAME = os.getenv("PADDED_POISONED_PIC_FILE_NAME")
 
 
 def nearest_lower_exponent_of_2(n):
@@ -18,11 +29,11 @@ def nearest_lower_exponent_of_2(n):
         return 0
     power = 1
     while power <= n:
-        power <<= 1  # This is equivalent to power = power * 2
-    return power >> 1  # Shift right to get the nearest lower power of 2
+        power <<= 1  # -> power = power * 2
+    return power >> 1  # Shift right to get the nearest lower power of 2 | binary byte shifting man
 
 
-class AutoEncoderHiddenLayerDimensionIterator:
+class AutoEncoderHiddenLayerDimensionIterator: # Helps you iterate thru automatic scaling of neural net just once
     def __init__(self, current_dim, input_dim, scaling_rate, input_to_waist_ratio):
         self.__current_dim = current_dim
 
@@ -41,7 +52,7 @@ class AutoEncoderHiddenLayerDimensionIterator:
         else:
             raise StopIteration
 
-class AutoEncoder(models.Model):
+class AutoEncoder(Model):
     def __init__(self, input_dim, latent_dim_scaling_rate, input_to_waist_ratio, add_reconstruction_loss: bool = True, variational: bool = True):
         super(AutoEncoder, self).__init__()
         self.__latent_dim = nearest_lower_exponent_of_2(input_dim[0]) # size of layer right after the input layer
@@ -51,7 +62,7 @@ class AutoEncoder(models.Model):
         self.__hidden_layer_dimensions.extend(
             [
                 hidden_dim for hidden_dim in AutoEncoderHiddenLayerDimensionIterator(
-                    current_dim=self.__latent_dim, input_dim=input_dim, 
+                    current_dim=self.__latent_dim, input_dim=input_dim[0], 
                     scaling_rate=latent_dim_scaling_rate, input_to_waist_ratio=input_to_waist_ratio
                 )
             ]
@@ -61,57 +72,145 @@ class AutoEncoder(models.Model):
         self.__variational = variational
 
         # Encoder
-        self.encoder = tf.keras.Sequential([
-            layers.InputLayer(input_shape=(input_dim,)),
-            layers.Dense(hidden_layer_dim, activation='relu') for hidden_layer_dim in self.__hidden_layer_dimensions
-        ])
+        self.__input = Input(shape=input_dim) #Input Layer
+        self.__encoder = [
+            Dense(hidden_layer_dim, activation='relu') for hidden_layer_dim in self.__hidden_layer_dimensions
+            ] 
 
         # Decoder
-        decoder = [
-            layers.Dense(hidden_layer_dim, activation='relu') for hidden_layer_dim in reversed(self.__hidden_layer_dimensions)
-        ]
-        decoder.append(layers.Dense(input_dim, activation='sigmoid'))
-        self.decoder = tf.keras.Sequential(decoder)
+        self.__decoder = [
+            Dense(hidden_layer_dim, activation='relu') for hidden_layer_dim in reversed(self.__hidden_layer_dimensions)
+            ]
+        self.__output = Dense(input_dim, activation='sigmoid')
 
-    def call(self, x):
-        # TODO is this lazy execution until compile is called?
+
+    def call(self, inputs, training=False):
         # Encode
-        encoded = self.encoder(x)
-        
+        z = self.__input(inputs)
+        for encode_layer in self.__encoder:
+            z = encode_layer(z)
+
         # Add Random Noise to make it Variational & Set Decoder
         if self.__variational:
-            mean, log_var = tf.split(encoded, num_or_size_splits=2, axis=1)
+            mean, log_var = tf.split(z, num_or_size_splits=2, axis=1)
             epsilon = tf.random.normal(shape=(tf.shape(mean)[0], self.__next_dim))
             z = mean + tf.exp(log_var * 0.5) * epsilon
-            reconstructed = self.decoder(z)
-        else:
-            reconstructed = self.decoder(encoded)
+
+        # Decode
+        for decode_layer in self.__decoder:
+            z = decode_layer(z)
+        z = self.__output(z)
+        
+            
         
         # Loss Function
         kl_loss = -0.5 * tf.reduce_mean(log_var - tf.square(mean) - tf.exp(log_var) + 1)
         if self.__add_reconstruction_loss:
-            reconstruction_loss = tf.mean(tf.square(reconstructed - x))
+            reconstruction_loss = tf.mean(tf.square(z - x))
             self.add_loss(kl_loss + reconstruction_loss)
         else:
             self.add_loss(kl_loss)
         
-        return reconstructed
-
-
-# Example usage:
-input_dim = 784
-latent_dim = 2
-
-exp_map = {
-    "ae_kl_only": AutoEncoder(input_dim, latent_dim_scaling_rate=0.7, input_to_waist_ratio=4, add_reconstruction_loss=False, variational=False),
-    "vae_kl_only": AutoEncoder(input_dim, latent_dim_scaling_rate=0.7, input_to_waist_ratio=4, add_reconstruction_loss=False, variational=True),
-    "ae_kl_and_rl": AutoEncoder(input_dim, latent_dim_scaling_rate=0.7, input_to_waist_ratio=4, add_reconstruction_loss=True, variational=False),
-    "vae_kl_and_rl": AutoEncoder(input_dim, latent_dim_scaling_rate=0.7, input_to_waist_ratio=4, add_reconstruction_loss=True, variational=True)
-}
-
-for exp_type, exp in exp_map.items():
-    exp.compile(optimizer='adam')
-    exp.fit()
+        return z
 
 
 
+def main(args):
+    input_dim = None
+    # Grab resized photos
+    X_resized_train, Y_resized_train, X_resized_test = [], [], []
+    y_names = set()
+    for file in os.listdir(POISON_PIC_LOCATION + RESIZED_POISONED_PIC_FILE_NAME):
+        file_path = os.path.join(POISON_PIC_LOCATION + RESIZED_POISONED_PIC_FILE_NAME, file)
+        if file_path.split(".")[-1] in ok_pic_formats:
+            pic_name = file_path.split("/")[-1]
+            y_names.add(pic_name)
+            image = img.import_image(file_path)
+            image = img.resize_image(image, args.new_width, args.new_height)
+            Y_resized_train.append(image)
+    
+    for file in os.listdir(REGULAR_PIC_LOCATION + RESIZED_REGULAR_PIC_FILE_NAME):
+        file_path = os.path.join(REGULAR_PIC_LOCATION + RESIZED_REGULAR_PIC_FILE_NAME, file)
+        if file_path.split(".")[-1] in ok_pic_formats:
+            pic_name = file_path.split("/")[-1]
+            image = img.import_image(file_path)
+            image = img.resize_image(image, args.new_width, args.new_height)
+            input_dim = image.shape
+            if pic_name in y_names:
+                X_resized_train.append(image)
+            else:
+                X_resized_test.append(image)
+    
+    # Grab padded photos
+    # X_padded, Y_padded = [], []
+    # for file in os.listdir(REGULAR_PIC_LOCATION + PADDED_REGULAR_PIC_FILE_NAME):
+    #     file_path = os.path.join(REGULAR_PIC_LOCATION + PADDED_REGULAR_PIC_FILE_NAME, file)
+    #     if file_path.split(".")[-1] in ok_pic_formats:
+    #         pic_name = file_path.split("/")[-1]
+    #         image = img.import_image(file_path)
+    #         X_padded.append(X_padded)
+
+    # for file in os.listdir(POISON_PIC_LOCATION + PADDED_POISONED_PIC_FILE_NAME):
+    #     file_path = os.path.join(POISON_PIC_LOCATION + PADDED_POISONED_PIC_FILE_NAME, file)
+    #     if file_path.split(".")[-1] in ok_pic_formats:
+    #         pic_name = file_path.split("/")[-1]
+    #         image = img.import_image(file_path)
+    #         Y_padded.append(X_padded)
+
+    exp_map = {
+        "ae_kl_only": {
+            "add_reconstruction_loss": False,
+            "variational": False
+        },
+        "vae_kl_only": {
+            "add_reconstruction_loss": False, 
+            "variational": True
+        },
+        "ae_kl_and_rl": {
+            "add_reconstruction_loss": True, 
+            "variational": False
+        },
+        "vae_kl_and_rl": {
+            "add_reconstruction_loss": True, 
+            "variational": True
+        }
+    }
+    
+    for exp_type, params in exp_map.items():
+        model = AutoEncoder(
+            input_dim, latent_dim_scaling_rate=0.7, input_to_waist_ratio=4, 
+            add_reconstruction_loss=params["add_reconstruction_loss"], variational=params["variational"]
+        )
+        model.compile(optimizer='adam')
+        model.fit(
+            x=X_resized_train,
+            y=Y_resized_train,
+            epochs=5,
+            verbose="auto",
+            callbacks=None,
+            validation_split=0.0,
+            validation_data=None,
+            shuffle=True,
+            class_weight=None,
+            sample_weight=None,
+            initial_epoch=0,
+            steps_per_epoch=None,
+            validation_steps=None,
+            validation_batch_size=None,
+            validation_freq=1,
+        )
+        y_hat = model.predict(X_resized_test)
+
+        save_path = HOMEMADE_POISON_PIC_LOCATION + RESIZED_POISONED_PIC_FILE_NAME
+        for i, y in enumerate(y_hat):
+            cv2.imwrite(save_path + exp_type + "/pic{}".format(i), y)
+
+
+    # print(exp)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("-i", "--new_height", type=int, help="height of pic to resize to", default=512)
+    args.add_argument("-w", "--new_width", type=int, help="width of pic to resize to", default=512)
+    main(args.parse_args())
